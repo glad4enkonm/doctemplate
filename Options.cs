a0a81f72e179a45e9a49b2e503520cd1e4253882
Nikolai Gladchenko <glad4enkonm@gmail.com>
2020-04-01 10:55:59 +0200

    save values to yaml added, refactoring

---
+++ Options.cs
@@ -0,0 +1,16 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace doctemplate
+{
+    public class Options
+    {
+        [Option('t', "template", Required = true, HelpText = "Templates files to be processed.")]
+        public IEnumerable<string> TemplateFiles { get; set; }
+
+        [Option('s', "save", HelpText = "Save values to a yaml file.")]
+        public bool SaveYAML { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,20 +2,18 @@
 using DocumentFormat.OpenXml.Packaging;
 using Jint;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
+using YamlDotNet.Serialization;
 
 namespace doctemplate
 {
     class Program
     {
-        public class Options
-        {
-            [Option('t', "template", Required = true, HelpText = "Templates files to be processed.")]
-            public IEnumerable<string> TemplateFiles { get; set; }
-        }
+        private static Dictionary<string, string> EnteredKeyValuePairs = new Dictionary<string, string>();
 
         static void Main(string[] args)
         {
@@ -25,17 +23,8 @@ namespace doctemplate
 
         static void ProcessDocx(Options opts)
         {
-
-            string templatePath = opts.TemplateFiles.First();
-            string documentPath = templatePath.Replace("_", "output\\");
-            string documentText;
-            using (WordprocessingDocument wordDoc = WordprocessingDocument.Open(templatePath, false))
-            {
-                using (StreamReader reader = new StreamReader(wordDoc.MainDocumentPart.GetStream()))
-                {
-                    documentText = reader.ReadToEnd();
-                }
-            }
+            string templatePath, documentPath, documentText;
+            LoadDocxFile(opts, out templatePath, out documentPath, out documentText);
 
             var regex = new Regex(@"!([^!<>]+)!");
             var values = regex.Matches(documentText).Select(match => match.Groups.Values.Last().Value);
@@ -44,6 +33,9 @@ namespace doctemplate
             IEnumerable<string> constValues = valueSet.Where(m => !m.StartsWith("=")); // all not formulas
 
             Engine jsEngine = new Engine();
+
+            IEnumerable<string> loadedKeys = LoadValuesFromFileIfExist(ref documentText, templatePath, ref jsEngine);
+
             jsEngine = jsEngine.Execute( // define function today
                 @"function today(days) {
                     const daysToAdd = isNaN(days) ? 0 : days;
@@ -57,22 +49,36 @@ namespace doctemplate
                     return amount.toLocaleString('de - DE', {minimumFractionDigits: 2});
                 }");
 
-            Console.WriteLine("Enter template values:");
-            foreach (var constant in constValues)
+            IEnumerable<string> keysToEnter = constValues.Except(loadedKeys);
+            if (keysToEnter.Any())
             {
-                Console.Write($"{constant}=");
-                string value = Console.ReadLine();
-                jsEngine = jsEngine.SetValue(constant, value);
-                documentText = documentText.Replace($"!{constant}!", value);
+                Console.WriteLine("Enter template values:");
+                foreach (var constant in keysToEnter)
+                {
+                    Console.Write($"{constant}=");
+                    string value = Console.ReadLine();
+                    EnteredKeyValuePairs.Add(constant, value);
+                    jsEngine = jsEngine.SetValue(constant, value);
+                    documentText = documentText.Replace($"!{constant}!", value);
+                }
             }
 
             IEnumerable<string> funcValues = valueSet.Where(m => m.StartsWith("=")); // all not formulas
             foreach (var func in funcValues)
-            {
                 ProcessFunction(ref documentText, regex, ref jsEngine, func);
+
+            SaveDocxFile(templatePath, documentPath, documentText);
+
+            if (opts.SaveYAML)
+            {
+                var serializer = new SerializerBuilder().Build();
+                var yaml = serializer.Serialize(EnteredKeyValuePairs);
+                File.WriteAllText(templatePath + ".yaml", yaml);
             }
+        }
 
-
+        private static void SaveDocxFile(string templatePath, string documentPath, string documentText)
+        {
             // Create output directory and files
             FileInfo fileInfo = new FileInfo(templatePath);
             new FileInfo(documentPath).Directory.Create(); // create path
@@ -85,6 +91,38 @@ namespace doctemplate
                     writer.Write(documentText);
                 }
             }
+        }
+
+        private static void LoadDocxFile(Options opts, out string templatePath, out string documentPath, out string documentText)
+        {
+            templatePath = opts.TemplateFiles.First();
+            documentPath = templatePath.Replace("_", "output\\");
+            using (WordprocessingDocument wordDoc = WordprocessingDocument.Open(templatePath, false))
+            {
+                using (StreamReader reader = new StreamReader(wordDoc.MainDocumentPart.GetStream()))
+                {
+                    documentText = reader.ReadToEnd();
+                }
+            }
+        }
+
+        private static IEnumerable<string> LoadValuesFromFileIfExist(ref string documentText, string templatePath, ref Engine jsEngine)
+        {
+            string valuesFilePath = templatePath + ".yaml";
+            if (!File.Exists(valuesFilePath))
+                return new string[] { }; // return empty string array if no file
+            
+            string yaml = File.ReadAllText(valuesFilePath);
+
+            var deserializer = new Deserializer();
+            var yamlValues = deserializer.Deserialize<Dictionary<string, string>>(new StringReader(yaml));
+            foreach (var pair in yamlValues)
+            {
+                jsEngine = jsEngine.SetValue(pair.Key, pair.Value);
+                Console.WriteLine($"{pair.Key}={pair.Value}");
+                documentText = documentText.Replace($"!{pair.Key}!", pair.Value);
+            }
+            return yamlValues.Keys;
         }
 
         private static void ProcessFunction(ref string documentText, Regex regex, ref Engine jsEngine, string func)
@@ -107,6 +145,8 @@ namespace doctemplate
 
                 Console.Write($"{whatToDefine}=");
                 string value = Console.ReadLine();
+                EnteredKeyValuePairs.Add(whatToDefine, value);
+
                 jsEngine = jsEngine.SetValue(whatToDefine, value);
 
                 ProcessFunction(ref documentText, regex, ref jsEngine, func); // call recursively
